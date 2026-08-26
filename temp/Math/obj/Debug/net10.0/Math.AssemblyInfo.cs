d18611908109b83bf03a4167ab41af28d71143e6
yousa25 <2446814668@qq.com>
2026-08-26 17:39:34 +0800

    线性表上

--- temp/Math/obj/Debug/net10.0/Math.AssemblyInfo.cs
+++ temp/Math/obj/Debug/net10.0/Math.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Math")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26d600770413599c20b776164d00b26c49452317")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7e916a99f37df37f27b175f9dd05b47ebb9c016")]
 [assembly: System.Reflection.AssemblyProductAttribute("Math")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Math")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
