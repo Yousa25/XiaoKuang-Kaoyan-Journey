a429f46cd88c690611996d6f3214cbf3b6d4f8b9
yousa25 <2446814668@qq.com>
2026-08-27 13:34:24 +0800

    政治：马克思主义原理考点1和2

--- temp/Math/obj/Debug/net10.0/Math.AssemblyInfo.cs
+++ temp/Math/obj/Debug/net10.0/Math.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Math")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7e916a99f37df37f27b175f9dd05b47ebb9c016")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d18611908109b83bf03a4167ab41af28d71143e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Math")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Math")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
