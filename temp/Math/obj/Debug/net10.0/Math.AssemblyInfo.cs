42419f7c8cd4b99b4083d0d7ef6296442f62bc06
yousa25 <2446814668@qq.com>
2026-08-31 17:31:50 +0800

    math

--- temp/Math/obj/Debug/net10.0/Math.AssemblyInfo.cs
+++ temp/Math/obj/Debug/net10.0/Math.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Math")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b6c4681fcd157ffef5cdaafa2b80cff3b9d7345")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b7cfc2327760794fb040c508f61c400f81f56da")]
 [assembly: System.Reflection.AssemblyProductAttribute("Math")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Math")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
